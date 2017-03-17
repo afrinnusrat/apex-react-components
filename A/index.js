5237cd34b30ebc4b731cb76a63714dbe432293dc
Julie Petrarca <jpetrarca@apexclearing.com>
2017-03-17 14:04:47 -0700

    chore(rainbow): adds animation keyframes back to our rainbow

--- A/index.js
+++ A/index.js
@@ -2,7 +2,13 @@
  * A link to a certain page, an anchor tag
  */
 
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
+
+const rainbow = keyframes`
+  0%{background-position:0% 82%}
+  50%{background-position:100% 19%}
+  100%{background-position:0% 82%}
+`;
 
 const A = styled.a`
   color: #FFF;
@@ -12,12 +18,7 @@ const A = styled.a`
   text-align: center;
   background: linear-gradient(124deg, #ff2400, #e81d1d, #e8b71d, #e3e81d, #1de840, #1ddde8, #2b1de8, #dd00f3, #dd00f3);
   background-size: 1800% 1800%;
-  animation: rainbow 18s ease infinite;
-  @keyframes rainbow {
-      0%{background-position:0% 82%}
-      50%{background-position:100% 19%}
-      100%{background-position:0% 82%}
-  }
+  animation: {rainbow} 18s ease infinite;
 `;
 
 export default A;
